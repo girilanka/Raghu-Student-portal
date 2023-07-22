358fa1ae4eb65fe6fc88d404dd6e689adf575f18
Giri Nagendra Kumar Lanka <girinagendra1901@gmail.com>
2023-07-22 20:10:53 +0530

    This is Third Push

--- StudentPortal/settings.py
+++ StudentPortal/settings.py
@@ -117,9 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT =BASE_DIR /'static'
-SATICFILES_DIRS = [
-    'static',
+STATIC_ROOT = BASE_DIR /'static'
+STATICFILES_DIRS = [
+    'StudentPortal/static',
 ]
 
 
